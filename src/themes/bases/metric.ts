d896b4dbc24bf74aa68af85be03fdc11536c54ae
Kien Nguyen <kien.nguyen@asnet.com.vn>
2023-11-23 17:27:25 +0700

    feat: custom spaces and radii theme

---
+++ src/themes/bases/metric.ts
@@ -0,0 +1,18 @@
+import { ThemeOverride } from '@chakra-ui/react';
+
+// Custom spaces
+export const colors: ThemeOverride['spaces'] = {
+  10.5: '42px',
+  11: '44px',
+  11.5: '55px',
+  16.5: '50px',
+  36.5: '52px',
+};
+
+// Custom radii
+export const radii: ThemeOverride['radii'] = {
+  full: '9999px',
+  lg: '8px',
+  xl: '12px',
+  '2xl': '18px',
+};
