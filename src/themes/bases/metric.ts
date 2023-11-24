d433380ebef33bc54d7f1ec74a4960b53cf7907b
Kien Nguyen <kien.nguyen@asnet.com.vn>
2023-11-24 11:16:31 +0700

    fix: define custom spaces theme

--- src/themes/bases/metric.ts
+++ src/themes/bases/metric.ts
@@ -2,29 +2,11 @@ import { ThemeOverride } from '@chakra-ui/react';
 
 // Custom spaces
 export const spaces: ThemeOverride['spaces'] = {
-  0.5: '1px',
-  1.5: '9px',
-  2: '48px',
-  2.5: '25px',
-  3: '36px',
-  3.5: '49px',
-  4: '64px',
-  5: '100px',
-  6: '144px',
-  7: '196px',
-  8: '256px',
-  9: '324px',
-  10: '400px',
   10.5: '42px',
   11: '44px',
-  11.5: '55px',
-  12: '48px',
-  14: '56px',
-  16: '64px',
-  16.5: '50px',
-  20: '80px',
-  24: '96px',
-  36.5: '52px',
+  13.75: '55px',
+  19: '76px',
+  39: '156px',
 };
 
 // Custom radii
