849fd66a84a1e301700e4d5845c6bbf44dbe982e
Kien Nguyen <kien.nguyen@asnet.com.vn>
2023-11-24 10:39:33 +0700

    feat: update spaces theme

--- src/themes/bases/metric.ts
+++ src/themes/bases/metric.ts
@@ -2,10 +2,28 @@ import { ThemeOverride } from '@chakra-ui/react';
 
 // Custom spaces
 export const colors: ThemeOverride['spaces'] = {
+  0.5: '1px',
+  1.5: '9px',
+  2: '48px',
+  2.5: '25px',
+  3: '36px',
+  3.5: '49px',
+  4: '64px',
+  5: '100px',
+  6: '144px',
+  7: '196px',
+  8: '256px',
+  9: '324px',
+  10: '400px',
   10.5: '42px',
   11: '44px',
   11.5: '55px',
+  12: '48px',
+  14: '56px',
+  16: '64px',
   16.5: '50px',
+  20: '80px',
+  24: '96px',
   36.5: '52px',
 };
 
