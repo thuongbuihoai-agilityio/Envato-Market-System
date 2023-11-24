54add906fec891f8e46590c022f959eb0d42628c
Kien Nguyen <kien.nguyen@asnet.com.vn>
2023-11-24 13:59:22 +0700

    fix: styles define for metrics

--- src/themes/bases/metric.ts
+++ src/themes/bases/metric.ts
@@ -2,11 +2,11 @@ import { ThemeOverride } from '@chakra-ui/react';
 
 // Custom spaces
 export const spaces: ThemeOverride['spaces'] = {
-  10.5: '42px',
-  11: '44px',
-  13.75: '55px',
-  19: '76px',
-  39: '156px',
+  '10.5': '42px',
+  '11': '44px',
+  '13.75': '55px',
+  '19': '76px',
+  '39': '156px',
 };
 
 // Custom radii
--- src/themes/index.ts
+++ src/themes/index.ts
@@ -1,3 +1,7 @@
 import { extendTheme } from '@chakra-ui/react';
+import { spaces, radii } from './bases/metric';
 
-export const configThemes = extendTheme({});
+export const configThemes = extendTheme({
+  radii,
+  spaces,
+});
