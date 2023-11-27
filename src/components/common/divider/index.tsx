3b63362cfe7302d2def504a07bdc44271a274642
Kien Nguyen <kien.nguyen@asnet.com.vn>
2023-11-27 14:48:52 +0700

    fix: naming props

--- src/components/common/divider/index.tsx
+++ src/components/common/divider/index.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import { AbsoluteCenter, Box, Divider } from '@chakra-ui/react';
 
 interface Props {
-  children: string;
+  content: string;
 }
 
-const DividerCustom: React.FC<Props> = ({ children }) => (
+const DividerCustom: React.FC<Props> = ({ content }) => (
   <Box position="relative" w="full" m="auto" padding="10">
     <Divider w="full" />
     <AbsoluteCenter bg="white" px="4" color="gray.500">
-      {children}
+      {content}
     </AbsoluteCenter>
   </Box>
 );
