ce759c8a15907baca443d23c8922cd1f1fda6ed4
PhamNhatHuy <huy.phamnhat@asnet.com.vn>
2023-11-27 09:25:40 +0700

    feat: define baseStyle for fontSize of common button component

--- src/themes/components/Button.ts
+++ src/themes/components/Button.ts
@@ -2,24 +2,23 @@ import { defineStyleConfig } from '@chakra-ui/react';
 
 // The default style for Button component
 export const Button = defineStyleConfig({
+  baseStyle: {
+    fontSize: 'md',
+  },
   sizes: {
     sm: {
-      fontSize: 'md',
       py: 3,
       px: 7,
     },
     md: {
-      fontSize: 'md',
       py: 3,
       px: 10,
     },
     lg: {
-      fontSize: 'md',
       py: 3,
       px: 11,
     },
     xl: {
-      fontSize: 'md',
       py: 4,
       width: '100%',
     },
