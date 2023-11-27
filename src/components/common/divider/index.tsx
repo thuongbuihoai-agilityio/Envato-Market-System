6670cf236e688358f39ac793bfd8ff29e1aa1337
Kien Nguyen <kien.nguyen@asnet.com.vn>
2023-11-27 15:00:11 +0700

    fix: naming props

--- src/components/common/divider/index.tsx
+++ src/components/common/divider/index.tsx
@@ -8,6 +8,7 @@ interface Props {
 const DividerCustom: React.FC<Props> = ({ content }) => (
   <Box position="relative" w="full" m="auto" padding="10">
     <Divider w="full" />
+
     <AbsoluteCenter bg="white" px="4" color="gray.500">
       {content}
     </AbsoluteCenter>
