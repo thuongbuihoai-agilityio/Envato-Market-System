8d367e053ccf74aec4adf7ceaacdb7ca3b7ab922
Thuong Bui Hoai <thuong.buihoai@asnet.com.vn>
2023-11-27 16:28:24 +0700

    feat: format code

--- src/constants/images.ts
+++ src/constants/images.ts
@@ -3,6 +3,10 @@ export const IMAGES = {
     url: '/icons/signin.svg',
     alt: 'This is sign in image'
   },
+  SIGN_UP: {
+    url: '/icons/signup.svg',
+    alt: 'This is sign in image'
+  },
   SQUARE: {
     url: '/icons/square.svg',
     alt: 'This is square image'
@@ -14,9 +18,5 @@ export const IMAGES = {
   DOTTED: {
     url: '/icons/dotted.svg',
     alt: 'This is dotted image'
-  },
-  SIGN_UP: {
-    url: '/icons/signup.svg',
-    alt: 'This is sign in image'
   },
 };
