cb46006c47c7d75a72f260d98c35c4c865464ebe
Kien Nguyen <kien.nguyen@asnet.com.vn>
2023-11-27 16:27:10 +0700

    fix: style component

--- src/components/common/divider/index.tsx
+++ src/components/common/divider/index.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
 import { AbsoluteCenter, Box, Divider } from '@chakra-ui/react';
 
 interface Props {
   content: string;
 }
 
-const DividerCustom: React.FC<Props> = ({ content }) => (
+const DividerCustom = ({ content }: Props) => (
   <Box position="relative" w="full" m="auto" padding="10">
     <Divider w="full" />
 
