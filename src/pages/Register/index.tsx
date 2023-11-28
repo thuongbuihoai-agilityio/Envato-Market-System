e3d699c1dcda99f294178718a1eb32942aa71ba7
duong.pham <duong.pham@asnet.com.vn>
2023-11-28 15:33:20 +0700

    feat: add handle show/hide password

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,6 +1,14 @@
-import { memo } from 'react';
-import { Button, Checkbox, Flex, HStack, Text, VStack } from '@chakra-ui/react';
-import { ViewOffIcon } from '@chakra-ui/icons';
+import { FormEvent, memo, useCallback } from 'react';
+import {
+  Button,
+  Checkbox,
+  Flex,
+  HStack,
+  Text,
+  VStack,
+  useDisclosure,
+} from '@chakra-ui/react';
+import { ViewOffIcon, ViewIcon } from '@chakra-ui/icons';
 import { Link } from 'react-router-dom';
 
 // Constants
@@ -12,87 +20,114 @@ import { AuthLayout } from '@layouts/index';
 // Components
 import { InputField } from '@components/index';
 
-const RegisterPage = () => (
-  <AuthLayout isSignForm={false}>
-    <VStack as="form" gap={4} mb={6}>
-      <HStack
-        gap={{
-          base: 4,
-          md: 10,
-        }}
-        w="100%"
-        flexDirection={{
-          base: 'column',
-          md: 'row',
-        }}
+const RegisterPage = () => {
+  const { isOpen: isShowPassword, onToggle: onShowPassword } = useDisclosure();
+  const { isOpen: isShowConfirmPassword, onToggle: onShowConfirmPassword } =
+    useDisclosure();
+
+  const renderPasswordIcon = useCallback(
+    (isCorrect: boolean, callback: typeof onShowPassword): JSX.Element => {
+      const Icon = isCorrect ? ViewIcon : ViewOffIcon;
+
+      return (
+        <Icon
+          color="gray.400"
+          w="25px"
+          h="25px"
+          cursor="pointer"
+          onClick={callback}
+        />
+      );
+    },
+    [],
+  );
+
+  return (
+    <AuthLayout isSignInForm={false}>
+      <VStack
+        as="form"
+        gap={4}
+        mb={6}
+        onSubmit={(e: FormEvent) => e.preventDefault()}
       >
+        <HStack
+          gap={{
+            base: 4,
+            md: 10,
+          }}
+          w="100%"
+          flexDirection={{
+            base: 'column',
+            md: 'row',
+          }}
+        >
+          <InputField
+            variant="authentication"
+            placeholder="First name"
+            onChange={() => {}} // TODO: Will update when API integrate
+          />
+          <InputField
+            variant="authentication"
+            placeholder="Last name"
+            onChange={() => {}} // TODO: Will update when API integrate
+          />
+        </HStack>
         <InputField
           variant="authentication"
-          placeholder="First name"
+          placeholder="Email"
           onChange={() => {}} // TODO: Will update when API integrate
         />
         <InputField
+          type="password"
           variant="authentication"
-          placeholder="Last name"
+          placeholder="Password"
+          rightIcon={renderPasswordIcon(isShowPassword, onShowPassword)}
           onChange={() => {}} // TODO: Will update when API integrate
         />
-      </HStack>
-      <InputField
-        variant="authentication"
-        placeholder="Email"
-        onChange={() => {}} // TODO: Will update when API integrate
-      />
-      <InputField
-        type="password"
-        variant="authentication"
-        placeholder="Password"
-        rightIcon={
-          <ViewOffIcon color="gray.400" w="25px" h="25px" cursor="pointer" />
-        }
-        onChange={() => {}} // TODO: Will update when API integrate
-      />
-      <InputField
-        type="password"
-        variant="authentication"
-        placeholder="Confirm password"
-        rightIcon={
-          <ViewOffIcon color="gray.400" w="25px" h="25px" cursor="pointer" />
-        }
-        onChange={() => {}} // TODO: Will update when API integrate
-      />
-    </VStack>
+        <InputField
+          type="password"
+          variant="authentication"
+          placeholder="Confirm password"
+          rightIcon={renderPasswordIcon(
+            isShowConfirmPassword,
+            onShowConfirmPassword,
+          )}
+          onChange={() => {}} // TODO: Will update when API integrate
+        />
+      </VStack>
 
-    <Flex gap={3} my={7}>
-      <Checkbox size="md" colorScheme="green" defaultChecked></Checkbox>
-      <Text color="text.secondary" fontSize="md" flex={1}>
-        By creating an account, you agreeing to our
-        <Text as="span" color="text.primary">
-          Privacy Policy
+      <Flex gap={3} my={7}>
+        <Checkbox size="md" colorScheme="green" defaultChecked></Checkbox>
+        <Text color="text.secondary" fontSize="md" flex={1}>
+          By creating an account, you agreeing to our
+          <Text as="span" color="text.primary">
+            Privacy Policy
+          </Text>
+          , and
+          <Text as="span" color="text.primary">
+            Electronics Communication Policy.
+          </Text>
         </Text>
-        , and
-        <Text as="span" color="text.primary">
-          Electronics Communication Policy.
+      </Flex>
+
+      <Button textTransform="capitalize" my={7}>
+        Sign Un
+      </Button>
+
+      <Text fontWeight="medium" textAlign="center">
+        Already have an account?
+        <Text
+          as={Link}
+          to={ROUTES.REGISTER}
+          fontWeight="semibold"
+          textDecoration="underline"
+        >
+          Sign In
         </Text>
       </Text>
-    </Flex>
-
-    <Button textTransform="capitalize" my={7}>
-      Sign Un
-    </Button>
-
-    <Text fontWeight="medium" textAlign="center">
-      Already have an account?
-      <Text
-        as={Link}
-        to={ROUTES.REGISTER}
-        fontWeight="semibold"
-        textDecoration="underline"
-      >
-        Sign In
-      </Text>
-    </Text>
-  </AuthLayout>
-);
+    </AuthLayout>
+  );
+};
 
 const Register = memo(RegisterPage);
 export default Register;
