3afe9f11d8cd3dd3972eeb7ead4bf8e693310d08
duong.pham <duong.pham@asnet.com.vn>
2023-11-28 12:50:51 +0700

    feat: custom divider component

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export { default as InputField } from './common/InputField';
 export { default as Logo } from './common/Logo';
 export { default as Benefit } from './common/Benefit';
 export { default as Button } from './common/Button';
+export { default as Divider } from './common/Divider';
