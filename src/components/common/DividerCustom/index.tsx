38614d9ffe8b0c25dd6f6001d2cd47ba6be56e85
Kien Nguyen <kien.nguyen@asnet.com.vn>
2023-11-28 08:55:57 +0700

    fix: number property for padding

--- src/components/common/DividerCustom/index.tsx
+++ src/components/common/DividerCustom/index.tsx
@@ -5,10 +5,10 @@ interface DividerProps {
 }
 
 const DividerCustom = ({ content }: DividerProps) => (
-  <Box position="relative" w="full" m="auto" py="10">
+  <Box position="relative" w="full" m="auto" py={10}>
     <Divider w="full" h="1px" bg="gray.500" />
 
-    <AbsoluteCenter bg="white" px="4" color="gray.500">
+    <AbsoluteCenter bg="white" px={4} color="gray.500">
       {content}
     </AbsoluteCenter>
   </Box>
