79bb89c8f1358772d7a709f7de66ade833d3828e
duong.pham <duong.pham@asnet.com.vn>
2023-11-29 16:23:47 +0700

    feat: update default mode revalidate for React Hook Form

---
+++ src/hooks/useForm.ts
@@ -0,0 +1,18 @@
+import {
+  FieldValues,
+  UseFormProps,
+  useForm as useReactHookForm,
+} from 'react-hook-form';
+
+export const useForm = <
+  TFieldValues extends FieldValues = FieldValues,
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  TContext = any,
+  TTransformedValues extends FieldValues | undefined = undefined,
+>(
+  option?: UseFormProps<TFieldValues, TContext>,
+) =>
+  useReactHookForm<TFieldValues, TContext, TTransformedValues>({
+    ...option,
+    reValidateMode: 'onSubmit',
+  });
