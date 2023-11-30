36294921c75582c6797887171aa71b5517e4ff98
Thuong Bui Hoai <thuong.buihoai@asnet.com.vn>
2023-11-30 08:57:08 +0700

    feat: add new icon

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,4 @@ export { default as Logo } from './common/Logo';
 export { default as Benefit } from './common/Benefit';
 export { default as Button } from './common/Button';
 export { default as Divider } from './common/Divider';
+export { default as IconButton } from './common/IconButton';
