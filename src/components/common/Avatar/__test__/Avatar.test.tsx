39880c509e6e7974d42fe34719d91f49131f7928
Thuong Bui Hoai <thuong.buihoai@asnet.com.vn>
2023-11-30 11:20:32 +0700

    feat: add test snapshot

--- src/components/common/Avatar/__test__/Avatar.test.tsx
+++ src/components/common/Avatar/__test__/Avatar.test.tsx
@@ -6,6 +6,11 @@ import { Avatar } from '@components/index';
 describe('Avatar render', () => {
   afterEach(cleanup);
 
+  test('renders Benefit with content', () => {
+    const { container } = render(<Avatar />);
+    expect(container).toMatchSnapshot();
+  });
+
   it('should render avatar component', () => {
     const { getByTestId } = render(<Avatar data-testid="TestAvatar" />);
 
