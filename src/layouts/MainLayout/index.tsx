a3c1f18bd325141a9e9fc85936711a54ddc5c60c
duong.pham <duong.pham@asnet.com.vn>
2023-11-30 17:11:42 +0700

    feat: handle authentication flow

--- src/layouts/MainLayout/index.tsx
+++ src/layouts/MainLayout/index.tsx
@@ -1,11 +1,25 @@
-import { Outlet } from 'react-router-dom';
+import { Navigate, Outlet } from 'react-router-dom';
 import { Spinner } from '@chakra-ui/react';
 import { Suspense } from 'react';
 
-const MainLayout = () => (
-  <Suspense fallback={<Spinner />}>
-    <Outlet />
-  </Suspense>
-);
+// Hooks
+import { useAuth } from '@hooks/useAuth';
 
+// Constants
+import { ROUTES } from '@constants/index';
+
+// Types
+import { TUser } from '@interfaces/user';
+
+const MainLayout = () => {
+  const user = useAuth((state): TUser | null => state.user);
+
+  if (!user) return <Navigate to={ROUTES.LOGIN} />;
+
+  return (
+    <Suspense fallback={<Spinner />}>
+      <Outlet />
+    </Suspense>
+  );
+};
 export default MainLayout;
