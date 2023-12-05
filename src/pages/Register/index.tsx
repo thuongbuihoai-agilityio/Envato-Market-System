d4dfdd0b6493e746f91e1ab898c481177bf75440
duong.pham <duong.pham@asnet.com.vn>
2023-12-05 17:17:22 +0700

    fix: refactor rule

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -86,7 +86,7 @@ const RegisterPage = () => {
       setIsSubmit(true);
 
       // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      const { isAcceptPrivacyPolicy, ...fieldValues } = data;
+      const { isAcceptPrivacyPolicy, confirmPassword, ...fieldValues } = data;
       try {
         const { errors } = await signUp(fieldValues);
 
