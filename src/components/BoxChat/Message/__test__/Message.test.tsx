d498f49bdc86ec06a6274af0e554cb30e483144a
Kien Nguyen <kien.nguyen@asnet.com.vn>
2023-12-05 10:06:39 +0700

    feat: storybook and testing for Box Chat components

---
+++ src/components/BoxChat/Message/__test__/Message.test.tsx
@@ -0,0 +1,56 @@
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
+// Constants
+import { IMAGES } from '@constants/images';
+
+// Message
+import Message from '@components/BoxChat/Message';
+
+describe('Message component', () => {
+  it('renders correctly', () => {
+    const { container } = render(
+      <Message
+        content="This is message"
+        avatar={IMAGES.CHAT_USER_AVATAR.url}
+        isImage
+        avatarPosition="before"
+        isOwnerMessage
+      />,
+    );
+    expect(container).toMatchSnapshot();
+  });
+
+  it('should render image container when isImage is true', () => {
+    const { getByTestId } = render(
+      <Message content="Hello" avatar="avatar.png" isImage={true} />,
+    );
+
+    const imageContainer = getByTestId('image-container');
+    expect(imageContainer).toBeInTheDocument();
+  });
+
+  it('should render text content when isImage is false', () => {
+    const { getByTestId } = render(
+      <Message content="Hello" avatar="avatar.png" isImage={false} />,
+    );
+
+    const textContent = getByTestId('text-content');
+    expect(textContent).toBeInTheDocument();
+  });
+
+  it('should render avatar after the content when avatarPosition is "after"', () => {
+    const { getByTestId } = render(
+      <Message
+        content="Hello"
+        avatar="avatar.png"
+        isImage={false}
+        avatarPosition="after"
+      />,
+    );
+
+    const avatar = getByTestId('avatar');
+    expect(avatar).toBeInTheDocument();
+    expect(avatar).toHaveStyle('margin-left: 2px;');
+  });
+});
