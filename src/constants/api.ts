5abe0758569417700964102d93eee1a275bf6b2d
CanhVo <canh.vo@asnet.com.vn>
2023-12-07 17:07:46 +0700

    feat: integration api for overall balance

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -11,6 +11,7 @@ export const END_POINTS = {
   STATISTICS: '/statistics',
   REVENUE: '/revenue',
   EFFICIENCY: '/efficiency',
+  OVERALL_BALANCE: '/overall-balance',
 };
 
 export const SEARCH_PARAM = {
