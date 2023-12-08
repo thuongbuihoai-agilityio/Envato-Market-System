3170d00a7df56ec72b2c475ec46f287cb29c4e3e
Thuong Bui Hoai <thuong.buihoai@asnet.com.vn>
2023-12-08 10:28:13 +0700

    feat: integrate api for history table

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -4,29 +4,33 @@ import { Flex, Grid, GridItem } from '@chakra-ui/react';
 // Components
 import { BoxChat, CartPayment, HistoryTable } from '@components/index';
 
-// Constants
-import { HISTORY } from '@constants/history';
+// Hooks
+import { useTransaction } from '@hooks/useTransaction';
 
-const HistoryPage = () => (
-  <Grid
-    bg="background.body.primary"
-    py={12}
-    px={{ base: 6, xl: 12 }}
-    templateColumns={{ base: 'repeat(1, 1fr)', '2xl': 'repeat(4, 1fr)' }}
-    display={{ sm: 'block', md: 'grid' }}
-    gap={{ base: 0, '2xl': 12 }}
-  >
-    <GridItem colSpan={3}>
-      <HistoryTable histories={HISTORY} />
-    </GridItem>
-    <GridItem mt={{ base: 6, '2xl': 0 }}>
-      <Flex direction={{ base: 'column', lg: 'row', xl: 'column' }} gap={6}>
-        <CartPayment />
-        <BoxChat />
-      </Flex>
-    </GridItem>
-  </Grid>
-);
+const HistoryPage = () => {
+  const { data: transactions = [] } = useTransaction();
+
+  return (
+    <Grid
+      bg="background.body.primary"
+      py={12}
+      px={{ base: 6, xl: 12 }}
+      templateColumns={{ base: 'repeat(1, 1fr)', '2xl': 'repeat(4, 1fr)' }}
+      display={{ sm: 'block', md: 'grid' }}
+      gap={{ base: 0, '2xl': 12 }}
+    >
+      <GridItem colSpan={3}>
+        <HistoryTable histories={transactions} />
+      </GridItem>
+      <GridItem mt={{ base: 6, '2xl': 0 }}>
+        <Flex direction={{ base: 'column', lg: 'row', xl: 'column' }} gap={6}>
+          <CartPayment />
+          <BoxChat />
+        </Flex>
+      </GridItem>
+    </Grid>
+  );
+};
 
 const History = memo(HistoryPage);
 export default History;
