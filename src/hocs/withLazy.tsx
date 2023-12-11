06f0ce5b75fed134d4fd14e63644d4317730d227
Thuong Bui Hoai <thuong.buihoai@asnet.com.vn>
2023-12-11 15:35:14 -0800

    feat: update indicator center when logout

--- src/hocs/withLazy.tsx
+++ src/hocs/withLazy.tsx
@@ -5,7 +5,7 @@ const withLazy = <TProps extends object>(
   Component: FunctionComponent<TProps>,
 ): FunctionComponent<TProps> => {
   const ComponentLazy = (props: TProps): JSX.Element => (
-    <Suspense fallback={<Spinner />}>
+    <Suspense fallback={<Spinner position="fixed" top="50%" left="50%" />}>
       <Component {...props} />
     </Suspense>
   );
