19f125f3f3f2434e8234656e243c93593d26d4b2
duong.pham <duong.pham@asnet.com.vn>
2023-12-14 15:12:30 +0700

    refactor: refactor miini sidebar

--- src/components/MiniSidebar/index.tsx
+++ src/components/MiniSidebar/index.tsx
@@ -6,129 +6,123 @@ import { SidebarProps } from '@app/layouts/Sidebar';
 
 // components
 import {
+  Center,
+  ColorMode,
   Drawer,
   DrawerBody,
   DrawerContent,
   DrawerHeader,
-  Flex,
   Image,
   List,
   VStack,
+  useColorMode,
 } from '@chakra-ui/react';
 import { Menu } from '@app/components';
 
 // constants
 import {
+  IMAGES,
   HELP_ITEM_LIST,
   MENU_ITEM_LIST,
   OTHER_ITEM_LIST,
   SIDEBAR,
-} from '@app/constants/sidebar';
-import { IMAGES } from '@app/constants/images';
+} from '@app/constants';
 
-const MiniSidebar = ({ onClose, isOpen }: Omit<SidebarProps, 'onOpen'>) => (
-  <Drawer
-    placement="left"
-    onClose={onClose}
-    isOpen={isOpen}
-    closeOnOverlayClick={false}
-    trapFocus={false}
-    variant="clickThrough"
-    blockScrollOnMount={false}
-  >
-    <DrawerContent
-      maxW={SIDEBAR.MINI_SIDEBAR_WIDTH}
-      maxH="full"
-      display={{
-        base: 'none',
-        md: 'block',
-      }}
-      bg="background.component.primary"
+// Types
+import { TImage } from '@app/interfaces';
+
+const MiniSidebar = ({ onClose, isOpen }: Omit<SidebarProps, 'onOpen'>) => {
+  const { colorMode } = useColorMode();
+  const logos: Record<ColorMode, TImage> = {
+    light: IMAGES.LOGO_MINI_LIGHT,
+    dark: IMAGES.LOGO_MINI_DARK,
+  };
+
+  return (
+    <Drawer
+      placement="left"
+      onClose={onClose}
+      isOpen={isOpen}
+      closeOnOverlayClick={false}
+      trapFocus={false}
+      variant="clickThrough"
+      blockScrollOnMount={false}
     >
-      <DrawerHeader
-        display="flex"
-        alignItems="center"
-        justifyContent="center"
-        h={108}
-        borderRightColor="border.secondary"
-        borderRightWidth="1px"
-        borderBottomColor="border.secondary"
-        borderBottomWidth="1px"
-      >
-        {/* Mini Logo in Light Mode */}
-        <Image
-          src={IMAGES.LOGO_MINI_LIGHT.url}
-          alt={IMAGES.LOGO_MINI_LIGHT.alt}
-          _dark={{
-            display: 'none',
-          }}
-          cursor="pointer"
-        />
-
-        {/* Mini Logo in Dark Mode */}
-        <Image
-          src={IMAGES.LOGO_MINI_DARK.url}
-          alt={IMAGES.LOGO_MINI_DARK.alt}
-          _light={{
-            display: 'none',
-          }}
-          cursor="pointer"
-        />
-
-        {/* Close Button of Mini Sidebar */}
-        <Image
-          src={IMAGES.LEFT_ARROW.url}
-          alt={IMAGES.LEFT_ARROW.alt}
-          position="absolute"
-          top="auto"
-          transform="rotate(180deg)"
-          right={-4}
-          onClick={onClose}
-          display={{
-            base: 'none',
-            lg: 'block',
-          }}
-          cursor="pointer"
-        />
-      </DrawerHeader>
-
-      <DrawerBody
-        w="full"
-        pt={3.5}
-        pb={50}
-        h="full"
-        sx={{
-          '&::-webkit-scrollbar': {
-            display: 'none',
-          },
-          '&::-webkit-scrollbar-thumb': {
-            display: 'none',
-          },
+      <DrawerContent
+        maxW={SIDEBAR.MINI_SIDEBAR_WIDTH}
+        maxH="full"
+        display={{
+          base: 'none',
+          md: 'block',
         }}
-        display="flex"
-        justifyContent="center"
+        bg="background.component.primary"
       >
-        <VStack>
-          <List>
-            <Menu listItem={[...MENU_ITEM_LIST, ...HELP_ITEM_LIST]} isMinify />
-            <Menu listItem={[...OTHER_ITEM_LIST]} isMinify />
-          </List>
+        <DrawerHeader
+          as={Center}
+          h={108}
+          borderRightColor="border.secondary"
+          borderRightWidth="1px"
+          borderBottomColor="border.secondary"
+          borderBottomWidth="1px"
+        >
+          {/* Mini Logo in Light Mode */}
+          <Image
+            src={logos[colorMode].url}
+            alt={logos[colorMode].alt}
+            cursor="pointer"
+          />
 
-          <Flex
-            justifyContent="center"
-            alignItems="center"
-            borderRadius="full"
-            border="1px solid white"
-            boxSize={10}
-            bg="primary.500"
-          >
-            <Image src={IMAGES.MONEY_BAG.url} alt={IMAGES.MONEY_BAG.alt} />
-          </Flex>
-        </VStack>
-      </DrawerBody>
-    </DrawerContent>
-  </Drawer>
-);
+          {/* Close Button of Mini Sidebar */}
+          <Image
+            src={IMAGES.LEFT_ARROW.url}
+            alt={IMAGES.LEFT_ARROW.alt}
+            position="absolute"
+            top="auto"
+            transform="rotate(180deg)"
+            right={-4}
+            onClick={onClose}
+            display={{
+              base: 'none',
+              lg: 'block',
+            }}
+            cursor="pointer"
+          />
+        </DrawerHeader>
+
+        <DrawerBody
+          w="full"
+          pt={3.5}
+          pb={50}
+          h="full"
+          sx={{
+            '&::-webkit-scrollbar': {
+              display: 'none',
+            },
+            '&::-webkit-scrollbar-thumb': {
+              display: 'none',
+            },
+          }}
+          display="flex"
+          justifyContent="center"
+        >
+          <VStack>
+            <List>
+              <Menu
+                listItem={[...MENU_ITEM_LIST, ...HELP_ITEM_LIST]}
+                isMinify
+              />
+              <Menu listItem={[...OTHER_ITEM_LIST]} isMinify />
+            </List>
+
+            <Center border="1px solid white" boxSize={10} bg="primary.500">
+              <Image src={IMAGES.MONEY_BAG.url} alt={IMAGES.MONEY_BAG.alt} />
+            </Center>
+          </VStack>
+        </DrawerBody>
+      </DrawerContent>
+    </Drawer>
+  );
+};
 
 const MiniSidebarComponent = memo(MiniSidebar, isEqual);
 export default MiniSidebarComponent;
