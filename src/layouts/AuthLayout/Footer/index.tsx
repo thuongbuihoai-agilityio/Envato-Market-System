04436f0f744b61551cec6ec77bd1af0982288303
duong.pham <duong.pham@asnet.com.vn>
2023-12-14 16:58:58 +0700

    refactor: main layout

--- src/layouts/AuthLayout/Footer/index.tsx
+++ src/layouts/AuthLayout/Footer/index.tsx
@@ -3,22 +3,18 @@ import { Box, Text } from '@chakra-ui/react';
 // utils
 import { getCurrentYear } from '@app/utils/time';
 
-const Footer = () => {
-  const currentYear = getCurrentYear();
-
-  return (
-    <Box mt={24}>
-      <Text
-        mt={6}
-        fontSize="sm"
-        variant="textSm"
-        textAlign="center"
-        _dark={{ color: 'secondary.700' }}
-      >
-        @ {currentYear} Bankco. All Rights Reserved.
-      </Text>
-    </Box>
-  );
-};
+const Footer = () => (
+  <Box mt={24}>
+    <Text
+      mt={6}
+      fontSize="sm"
+      variant="textSm"
+      textAlign="center"
+      _dark={{ color: 'secondary.700' }}
+    >
+      @ {getCurrentYear()} Bankco. All Rights Reserved.
+    </Text>
+  </Box>
+);
 
 export default Footer;
