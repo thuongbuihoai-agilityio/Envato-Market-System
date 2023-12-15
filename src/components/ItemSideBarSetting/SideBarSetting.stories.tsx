9df9032d4a7299d01f6b5ce3a05cf55f3ee8c04e
Kien Nguyen <kien.nguyen@asnet.com.vn>
2023-12-15 15:35:52 +0700

    fix: storybook sidebar setting

--- src/components/ItemSideBarSetting/SideBarSetting.stories.tsx
+++ src/components/ItemSideBarSetting/SideBarSetting.stories.tsx
@@ -2,17 +2,39 @@ import type { Meta, StoryObj } from '@storybook/react';
 
 // components
 import SidebarSetting from '@app/components/ItemSideBarSetting';
+import { AvatarSetting, Faq } from '@app/assets/icons';
 
 const meta: Meta<typeof SidebarSetting> = {
   title: 'Custom Components/SidebarSetting',
   tags: ['autodocs'],
   component: SidebarSetting,
+  argTypes: {
+    onClick: { action: 'clicked' },
+  },
 };
 
 export default meta;
 
 type Story = StoryObj<typeof SidebarSetting>;
 
-export const Default: Story = {
-  args: {},
+export const Primary: Story = {
+  args: {
+    id: '1',
+    title: 'Heading',
+    content: 'This is my item',
+    activeItemId: '1',
+    onClick: () => {},
+    children: <AvatarSetting />,
+  },
+};
+
+export const Secondary: Story = {
+  args: {
+    id: '1',
+    title: 'Heading',
+    content: 'This is my item',
+    activeItemId: '1',
+    onClick: () => {},
+    children: <Faq />,
+  },
 };
