622505beacec7b89b22df536358fa464b3b59db5
Kien Nguyen <kien.nguyen@asnet.com.vn>
2023-12-18 14:14:34 +0700

    fix: update message

--- src/constants/messages.ts
+++ src/constants/messages.ts
@@ -1,15 +1,15 @@
 export const ERROR_MESSAGES = {
   FIELD_REQUIRED: (fieldName: string) => `${fieldName} is required`,
-  EMAIL_INVALID: 'Wrong email format',
-  PASSWORD_NOT_MATCH: 'Password don’t match',
+  EMAIL_INVALID: 'Email is invalid',
+  PASSWORD_NOT_MATCH: "Password doesn't match",
   SOMETHING_ERROR: 'Something went wrong!!',
-  AUTH_INCORRECT: 'Email or password incorrect',
-  ACCOUNT_ALREADY_EXISTS: 'An account using this email address already exists',
+  AUTH_INCORRECT: 'Email or password is incorrect',
+  ACCOUNT_ALREADY_EXISTS: 'This email address is already in used',
   EMPTY_DATA: 'Data is the empty',
   PASS_WORD_SHORT: 'Password must be more than 8 characters',
   PASS_WORD_WEAK:
     'Password contains uppercase, lowercase and special characters',
-  PHONE_NUMBER_INVALID: 'Invalid phone number',
+  PHONE_NUMBER_INVALID: 'Phone number is invalid',
   UPDATE_FAIL: {
     title: 'Update failed',
     description: 'Your profile has not been updated successfully',
