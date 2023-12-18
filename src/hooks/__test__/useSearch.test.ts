cc1215315614be9d931ca8c260ee1dc167cdd94f
Kien Nguyen <kien.nguyen@asnet.com.vn>
2023-12-18 14:55:39 +0700

    feat: improve unit test for useTransaction and useSearch

---
+++ src/hooks/__test__/useSearch.test.ts
@@ -0,0 +1,41 @@
+import { act, renderHook } from '@testing-library/react';
+import * as reactRouterDom from 'react-router-dom';
+
+// Hooks
+import { useSearch } from '@app/hooks';
+
+jest.mock('react-router-dom', () => ({
+  useSearchParams: jest.fn(),
+}));
+
+describe('useSearch', () => {
+  const defaultParams = { param1: 'value1', param2: 'value2' };
+
+  beforeEach(() => {
+    jest
+      .spyOn(reactRouterDom, 'useSearchParams')
+      .mockReturnValue([new URLSearchParams(defaultParams), jest.fn()]);
+  });
+
+  it('should initialize with default parameters', () => {
+    const { result } = renderHook(() => useSearch(defaultParams));
+
+    expect(result.current.searchParam).toEqual(defaultParams);
+    expect(result.current.defaultQuery.toString()).toEqual(
+      'param1=value1&param2=value2',
+    );
+  });
+
+  it('should update search parameter when setSearchParam is called', () => {
+    const { result } = renderHook(() => useSearch(defaultParams));
+
+    act(() => {
+      result.current.setSearchParam('param1', 'value1');
+    });
+
+    expect(result.current.searchParam.param1).toEqual('value1');
+    expect(result.current.defaultQuery.toString()).toEqual(
+      'param1=value1&param2=value2',
+    );
+  });
+});
