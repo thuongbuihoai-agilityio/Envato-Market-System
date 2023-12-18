0591cccfdd1e8b0c10069a1eaff75a2b589661d4
duong.pham <duong.pham@asnet.com.vn>
2023-12-18 07:44:59 +0700

    test: update unit test for useEmployee

--- src/hooks/useEmployee.ts
+++ src/hooks/useEmployee.ts
@@ -6,7 +6,7 @@ import { getEmployees } from '@app/services';
 export const useEmployee = (searchParam = '') => {
   const query = useQuery({
     queryKey: ['', searchParam],
-    queryFn: ({ signal }) => getEmployees(searchParam, { signal }),
+    queryFn: () => getEmployees(searchParam),
   });
 
   // TODO: update return value later
