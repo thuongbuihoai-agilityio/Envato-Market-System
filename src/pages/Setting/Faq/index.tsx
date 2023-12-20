3cd5bbd848d005ed61cad322034bcd82c1bd4286
Thuong Bui Hoai <thuong.buihoai@asnet.com.vn>
2023-12-20 11:31:13 +0700

    feat: update spaces in Faq component

--- src/pages/Setting/Faq/index.tsx
+++ src/pages/Setting/Faq/index.tsx
@@ -1,5 +1,4 @@
 import { memo } from 'react';
-import { Box } from '@chakra-ui/react';
 
 // Constants
 import { FAQ_DATA } from '@app/constants';
@@ -7,13 +6,10 @@ import { FAQ_DATA } from '@app/constants';
 // Components
 import { FaqItem } from '@app/components';
 
-const FaqPageComponent = () => (
-  <Box>
-    {FAQ_DATA.map(({ id, question, answer }) => (
-      <FaqItem key={id} question={question} answer={answer} />
-    ))}
-  </Box>
-);
+const FaqPageComponent = () =>
+  FAQ_DATA.map(({ id, question, answer }) => (
+    <FaqItem key={id} question={question} answer={answer} />
+  ));
 
 const FaqPage = memo(FaqPageComponent);
 
