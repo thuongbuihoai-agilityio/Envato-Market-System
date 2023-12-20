ad4bb5ec8208c3720e90edd7807a1430e4bfff0b
CanhVo <canh.vo@asnet.com.vn>
2023-12-20 10:29:04 +0700

    fix: update test case for efficiency component

--- src/components/Efficiency/__test__/EfficiencyInfo.test.tsx
+++ src/components/Efficiency/__test__/EfficiencyInfo.test.tsx
@@ -1,39 +1,44 @@
 import { render } from '@testing-library/react';
-import * as chakra from '@chakra-ui/react';
 
 // Components
 import EfficiencyInfo from '../EfficiencyInfo';
 
 // Mocks
 import { EFFICIENCY_MOCK } from '@app/mocks';
+import * as chakra from '@chakra-ui/react';
 
 jest.mock('react-apexcharts', () => ({
   __esModule: true,
   default: () => <div>EfficiencyInfo component</div>,
 }));
 
-jest.mock('@chakra-ui/react');
+jest.mock('@chakra-ui/react', () => ({
+  ...jest.requireActual('@chakra-ui/react'),
+  useColorMode: jest.fn(),
+}));
 
 describe('EfficiencyInfo component', () => {
-  it('should render correctly', () => {
+  it('should render correctly with light mode', () => {
     jest.spyOn(chakra, 'useColorMode').mockReturnValue({
       colorMode: 'light',
-      setColorMode: jest.fn(),
       toggleColorMode: jest.fn(),
+      setColorMode: jest.fn(),
     });
     const { container } = render(<EfficiencyInfo {...EFFICIENCY_MOCK} />);
 
     expect(container).toMatchSnapshot();
+    expect(container.querySelector('path[fill="#1A202C"]')).toBeDefined();
   });
 
-  it('should render with dark mode ', () => {
+  it('should render correctly with dark mode', () => {
     jest.spyOn(chakra, 'useColorMode').mockReturnValue({
       colorMode: 'dark',
-      setColorMode: jest.fn(),
       toggleColorMode: jest.fn(),
+      setColorMode: jest.fn(),
     });
     const { container } = render(<EfficiencyInfo {...EFFICIENCY_MOCK} />);
 
     expect(container).toMatchSnapshot();
+    expect(container.querySelector('path[fill="#FFF"]')).toBeDefined();
   });
 });
--- src/components/Efficiency/__test__/Statistical.test.tsx
+++ src/components/Efficiency/__test__/Statistical.test.tsx
@@ -4,12 +4,17 @@ import { render } from '@testing-library/react';
 import Statistical from '../Statistical';
 import { STROKE_COLORS } from '@app/constants';
 
-describe('EfficiencyInfo component', () => {
-  it('should render correctly', () => {
+describe('Statistical component', () => {
+  it('Should render correctly', () => {
     const { container } = render(
       <Statistical color={STROKE_COLORS[0]} title="Spending" value={1234} />,
     );
-
     expect(container).toMatchSnapshot();
+  });
+  it('Should get title of statistical ', () => {
+    const { getByText } = render(
+      <Statistical color={STROKE_COLORS[0]} title="Spending" value={1234} />,
+    );
+    expect(getByText('Spending')).toBeDefined();
   });
 });
