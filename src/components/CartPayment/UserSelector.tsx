5d200d93046a5e23bbaa3d4003b526aedacf3ec7
duong.pham <duong.pham@asnet.com.vn>
2023-12-21 15:04:14 +0700

    refactor: check re-render

---
+++ src/components/CartPayment/UserSelector.tsx
@@ -0,0 +1,74 @@
+import { Box, Image, Select, Text } from '@chakra-ui/react';
+import { memo } from 'react';
+import { Controller } from 'react-hook-form';
+
+// Icons
+import { ChevronIcon } from '../Icons';
+
+// Constants
+import { IMAGES } from '@app/constants';
+
+// Components
+import { TTransferControl } from '.';
+
+const UserSelectorComponent = ({ control }: TTransferControl): JSX.Element => (
+  <>
+    <Text
+      fontWeight="bold"
+      color="text.primary"
+      fontSize="lg"
+      mb={3}
+      textTransform="capitalize"
+    >
+      quick transfer
+    </Text>
+
+    <Box position="relative">
+      <Controller
+        control={control}
+        name="userId"
+        render={() => (
+          <Select
+            size="lg"
+            sx={{
+              paddingLeft: '50px',
+            }}
+            borderColor="border.secondary"
+            color="text.primary"
+            icon={<ChevronIcon />}
+          >
+            <option value="debit" color="text.primary">
+              Debit
+            </option>
+          </Select>
+        )}
+      />
+
+      <Image
+        src={IMAGES.DEBIT_ICON.url}
+        alt={IMAGES.DEBIT_ICON.alt}
+        fallbackSrc={IMAGES.USER.url}
+        boxSize={6}
+        position="absolute"
+        left={5}
+        top="50%"
+        transform="translateY(-50%)"
+      />
+
+      <Text
+        sx={{
+          position: 'absolute',
+          right: 10,
+          top: '50%',
+          transform: 'translateY(-50%)',
+        }}
+        fontWeight="bold"
+        fontSize="sm"
+      >
+        $ 10,431
+      </Text>
+    </Box>
+  </>
+);
+
+export const UserSelector = memo(UserSelectorComponent);
