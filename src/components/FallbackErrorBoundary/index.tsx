9aa664e2e62fcf655d440dae11c05c80a6ec67eb
Huy Nhat Pham <huy.phamnhat@asnet.com.vn>
2023-12-26 09:14:14 +0700

    test: write unit test for ErrorComponent

--- src/components/FallbackErrorBoundary/index.tsx
+++ src/components/FallbackErrorBoundary/index.tsx
@@ -5,10 +5,20 @@ import isEqual from 'react-fast-compare';
 
 const ErrorComponent = ({ error }: FallbackProps): JSX.Element => (
   <VStack justifyContent="center" minH="calc(100vh - 200px)">
-    <Text fontSize="lg" fontWeight="bold" color="text.primary">
+    <Text
+      fontSize="lg"
+      fontWeight="bold"
+      color="text.primary"
+      data-testid="error-title"
+    >
       An error has been occurred!!
     </Text>
-    <Text fontSize="3xl" fontWeight="bold" color="danger.400">
+    <Text
+      fontSize="3xl"
+      fontWeight="bold"
+      color="danger.400"
+      data-testid="error-message"
+    >
       {error?.message || (error as string)}
     </Text>
   </VStack>
