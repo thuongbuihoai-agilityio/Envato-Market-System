dcbd1a9e10529785d6e5cb7dead2ab544f72f0a1
Kien Nguyen <kien.nguyen@asnet.com.vn>
2023-12-27 16:30:14 +0700

    fix: mock test for onChange, onKeyDown and onClick

--- src/components/BoxChat/__test__/BoxChat.test.tsx
+++ src/components/BoxChat/__test__/BoxChat.test.tsx
@@ -1,10 +1,46 @@
-import { render } from '@testing-library/react';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom';
 
 // Components
 import BoxChat from '@app/components/BoxChat';
 
-test('BoxChat component renders correctly', () => {
-  const { container } = render(<BoxChat />);
-  expect(container).toMatchSnapshot();
+describe('BoxChatComponent', () => {
+  test('BoxChat component renders correctly', () => {
+    const { container } = render(<BoxChat />);
+    expect(container).toMatchSnapshot();
+  });
+
+  test('calls handleSendMessage when the send button is clicked', () => {
+    const handleSendMessage = jest.fn();
+    render(<BoxChat onSendMessage={handleSendMessage} />);
+
+    const sendButton = screen.getByRole('button', { name: /send/i });
+    fireEvent.click(sendButton);
+
+    expect(handleSendMessage).toHaveBeenCalledTimes(1);
+  });
+
+  test('calls handleChangeValue when the input value changes', () => {
+    const handleChangeValue = jest.fn();
+    render(<BoxChat onChange={handleChangeValue} />);
+
+    const input = screen.getByPlaceholderText('Type your message here');
+    fireEvent.change(input, { target: { value: 'Test message' } });
+
+    expect(handleChangeValue).toHaveBeenCalledWith('Test message');
+  });
+
+  test('calls handleSendMessage when Enter key is pressed', () => {
+    const handleSendMessage = jest.fn();
+    render(<BoxChat onSendMessage={handleSendMessage} />);
+
+    const input = screen.getByPlaceholderText('Type your message here');
+    fireEvent.change(input, { target: { value: 'Test message' } });
+
+    fireEvent.keyDown(input, { key: 'Enter' });
+
+    waitFor(() => {
+      expect(handleSendMessage).toHaveBeenCalledWith('Test message');
+    });
+  });
 });
--- src/components/BoxChat/index.tsx
+++ src/components/BoxChat/index.tsx
@@ -1,4 +1,4 @@
-import { memo } from 'react';
+import { ChangeEvent, memo, useCallback } from 'react';
 import { Box, Image, Heading, Flex, Input, Button } from '@chakra-ui/react';
 
 // Constants
@@ -10,123 +10,142 @@ import Message from '@app/components/BoxChat/Message';
 // Mocks
 import { MESSAGE_TIME, MESSAGES } from '@app/mocks';
 
-const BoxChatComponent = (): JSX.Element => (
-  <Box w="full" bg="background.body.quaternary" borderRadius="lg">
-    <Flex
-      direction="row"
-      justifyContent="space-between"
-      alignItems="center"
-      justify="center"
-      borderBottom="1px solid"
-      borderBottomColor="border.tertiary"
-      padding="24px 26px"
-    >
-      <Heading
-        as="h3"
-        fontWeight="semibold"
-        color="text.primary"
-        fontSize="2xl"
-        textTransform="capitalize"
+export type BoxChatProps = {
+  onSendMessage?: () => void;
+  onChange?: (value: string) => void;
+};
+
+const BoxChatComponent = ({ onSendMessage, onChange }: BoxChatProps) => {
+  const handleChangeValue = useCallback(
+    (e: ChangeEvent<HTMLInputElement>): void => onChange?.(e.target.value),
+    [onChange],
+  );
+
+  const handleSendMessage = (): void => {
+    onSendMessage?.();
+  };
+
+  return (
+    <Box w="full" bg="background.body.quaternary" borderRadius="lg">
+      <Flex
+        direction="row"
+        justifyContent="space-between"
+        alignItems="center"
+        justify="center"
+        borderBottom="1px solid"
+        borderBottomColor="border.tertiary"
+        padding="24px 26px"
       >
-        team chat
-      </Heading>
-
-      <Flex gap={5}>
-        <Image
-          src={IMAGES.USER_AVATAR.url}
-          alt={IMAGES.USER_AVATAR.alt}
-          fallbackSrc={IMAGES.USER.url}
-          w="52px"
-          h={8}
-        />
-
-        <Button
-          aria-label="btn-icon-plus"
-          bg="secondary.800"
-          w={9}
-          h={9}
-          borderRadius="50%"
+        <Heading
+          as="h3"
+          fontWeight="semibold"
+          color="text.primary"
+          fontSize="2xl"
+          textTransform="capitalize"
         >
+          team chat
+        </Heading>
+
+        <Flex gap={5}>
           <Image
-            src="icons/plus.svg"
+            src={IMAGES.USER_AVATAR.url}
             alt={IMAGES.USER_AVATAR.alt}
             fallbackSrc={IMAGES.USER.url}
-            w={3.5}
-            h={3.5}
+            w="52px"
+            h={8}
           />
-        </Button>
+
+          <Button
+            aria-label="btn-icon-plus"
+            bg="secondary.800"
+            w={9}
+            h={9}
+            borderRadius="50%"
+          >
+            <Image
+              src="icons/plus.svg"
+              alt={IMAGES.USER_AVATAR.alt}
+              fallbackSrc={IMAGES.USER.url}
+              w={3.5}
+              h={3.5}
+            />
+          </Button>
+        </Flex>
       </Flex>
-    </Flex>
 
-    <Box padding={{ base: '24px 20px', lg: '38px 35px' }}>
-      {MESSAGES.map((message): JSX.Element => {
-        const { isSend, isAudio, uid, content } = message;
+      <Box padding={{ base: '24px 20px', lg: '38px 35px' }}>
+        {MESSAGES.map((message): JSX.Element => {
+          const { isSend, isAudio, uid, content } = message;
 
-        return (
-          <Message
-            key={uid}
-            content={content}
-            isImage={isAudio}
-            isOwnerMessage={isSend}
-            avatarPosition={
-              isSend ? AVATAR_POSITION.AFTER : AVATAR_POSITION.BEFORE
-            }
-            avatar={IMAGES.CHAT_USER_AVATAR.url}
-            localeTime={MESSAGE_TIME}
+          return (
+            <Message
+              key={uid}
+              content={content}
+              isImage={isAudio}
+              isOwnerMessage={isSend}
+              avatarPosition={
+                isSend ? AVATAR_POSITION.AFTER : AVATAR_POSITION.BEFORE
+              }
+              avatar={IMAGES.CHAT_USER_AVATAR.url}
+              localeTime={MESSAGE_TIME}
+            />
+          );
+        })}
+
+        <Flex justify="center" align="center">
+          <Box
+            border="1px solid"
+            borderColor="border.secondary"
+            p="0 20px"
+            mt={5}
+            borderRadius="lg"
+            w="full"
+          >
+            <Flex direction="row" alignItems="center">
+              <Image
+                src={IMAGES.ATTACH.url}
+                alt={IMAGES.ATTACH.alt}
+                w={4}
+                h={15}
+                cursor="pointer"
+              />
+
+              <Input
+                variant="authentication"
+                ml={5}
+                _dark={{
+                  border: 'none',
+                }}
+                sx={{ border: 'none', padding: 0 }}
+                placeholder="Type your message here"
+                onChange={handleChangeValue}
+              />
+
+              <Image
+                src={IMAGES.MICRO.url}
+                alt={IMAGES.MICRO.alt}
+                w={6}
+                h={6}
+                cursor="pointer"
+              />
+            </Flex>
+          </Box>
+          <Image
+            role="button"
+            mt={5}
+            ml={5}
+            src={IMAGES.SEND.url}
+            alt={IMAGES.SEND.alt}
+            w={5}
+            h={18}
+            cursor="pointer"
+            onClick={handleSendMessage}
           />
-        );
-      })}
-
-      <Flex justify="center" align="center">
-        <Box
-          border="1px solid"
-          borderColor="border.secondary"
-          p="0 20px"
-          mt={5}
-          borderRadius="lg"
-          w="full"
-        >
-          <Flex direction="row" alignItems="center">
-            <Image
-              src={IMAGES.ATTACH.url}
-              alt={IMAGES.ATTACH.alt}
-              w={4}
-              h={15}
-              cursor="pointer"
-            />
-
-            <Input
-              variant="authentication"
-              ml={5}
-              _dark={{
-                border: 'none',
-              }}
-              sx={{ border: 'none', padding: 0 }}
-              placeholder="Type your message here"
-            />
-
-            <Image
-              src={IMAGES.MICRO.url}
-              alt={IMAGES.MICRO.alt}
-              w={6}
-              h={6}
-              cursor="pointer"
-            />
-          </Flex>
-        </Box>
-        <Image
-          mt={5}
-          ml={5}
-          src={IMAGES.SEND.url}
-          alt={IMAGES.SEND.alt}
-          w={5}
-          h={18}
-          cursor="pointer"
-        />
-      </Flex>
+        </Flex>
+      </Box>
     </Box>
-  </Box>
-);
+  );
+};
 
 const BoxChat = memo(BoxChatComponent);
 
