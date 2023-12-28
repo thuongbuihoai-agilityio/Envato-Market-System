c7cebdd680eb56157a9c37ec6f3c85dd432598f7
thuong.buihoai <thuong.buihoai@asnet.com.vn>
2023-12-28 22:11:22 +0700

    feat: implement ui for button

--- src/components/common/Table/Body/ActionCell.tsx
+++ src/components/common/Table/Body/ActionCell.tsx
@@ -1,34 +1,50 @@
-import { IconButton, IconButtonProps, Td } from '@chakra-ui/react';
-import { memo } from 'react';
+import { Box, IconButton, IconButtonProps, Td } from '@chakra-ui/react';
+import { memo, useCallback, useState } from 'react';
 
 // Icons
 import { Dot } from '@app/components/Icons';
+import { DeleteIcon, EditIcon } from '@chakra-ui/icons';
 
-type TActionCellProps = Pick<IconButtonProps, 'onClick'>;
+const ActionCellComponent = () => {
+  const [openModal, setOpenModal] = useState<boolean>(false);
 
-const ActionCellComponent = ({ onClick }: TActionCellProps): JSX.Element => (
-  <Td
-    py={5}
-    px={0}
-    fontSize="md"
-    color="text.primary"
-    fontWeight="semibold"
-    textAlign="center"
-  >
-    <IconButton
-      aria-label="This is the icon action"
-      w={7}
-      h={7}
-      bgColor="transparent"
-      _hover={{
-        bgColor: 'transparent',
-      }}
-      onClick={onClick}
+  const handleToggle = useCallback(() => {
+    console.log('run');
+
+    setOpenModal(!openModal)
+  }, [openModal]);
+
+  return (
+    <Td
+      py={5}
+      px={0}
+      fontSize="md"
+      color="text.primary"
+      fontWeight="semibold"
+      textAlign="center"
+      position='relative'
     >
-      <Dot />
-    </IconButton>
-  </Td>
-);
+      <IconButton
+        aria-label="This is the icon action"
+        w={7}
+        h={7}
+        bgColor="transparent"
+        _hover={{
+          bgColor: 'transparent',
+        }}
+        onClick={handleToggle}
+      >
+        <Dot />
+      </IconButton>
+      {openModal &&
+        <Box position='absolute' w='fit-content' bottom={55} right={2}>
+          <EditIcon />
+          <DeleteIcon />
+        </Box>
+      }
+    </Td>
+  );
+}
 
 const ActionCell = memo(ActionCellComponent);
 
