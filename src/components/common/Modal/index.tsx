198b3062b73225cee01e05b97bc2e6e0db4d751e
Huy Nhat Pham <huy.phamnhat@asnet.com.vn>
2023-12-28 16:41:46 +0700

    feat: implement UI for pin code modal

---
+++ src/components/common/Modal/index.tsx
@@ -0,0 +1,38 @@
+import { memo } from 'react';
+import {
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalHeader,
+  ModalOverlay,
+} from '@chakra-ui/react';
+
+export type TModalProps = {
+  isOpen: boolean;
+  onClose: () => void;
+  title?: string;
+  renderBody?: () => JSX.Element;
+};
+
+const ModalComponent = ({
+  isOpen,
+  onClose,
+  renderBody = () => <></>,
+  title = '',
+}: TModalProps) => (
+  <Modal
+    isCentered
+    closeOnOverlayClick={false}
+    isOpen={isOpen}
+    onClose={onClose}
+  >
+    <ModalOverlay />
+    <ModalContent minW={320} maxW="fit-content">
+      <ModalHeader>{title}</ModalHeader>
+      <ModalBody>{renderBody()}</ModalBody>
+    </ModalContent>
+  </Modal>
+);
+
+const CustomModal = memo(ModalComponent);
+export default CustomModal;
